ca84a867295ce7933900e63c559c9ca31fef42fa
Marcin Rojek <marcin.rojek1985@gmail.com>
2018-07-17 13:41:39 +0200

    Changes in controller

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,31 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String helloWorld() {
-
-        return "FOR EVERYONE";
-    }
-
-    @GetMapping("table")
+    @GetMapping("")
     public String table1() {
 
         return "table1";
     }
-
-    @GetMapping("/admin")
-    @ResponseBody
-    public String admin(){
-
-        return "Admin panel";
-    }
-
-    @GetMapping("/remove")
-    @ResponseBody
-    public String remove(){
-
-        return "Remove";
-    }
-
 }
